fb27b09b9db985df65886ae2e845d54a3f749e4c
hyhwaxung <hyhwaxung@gmail.com>
2023-08-12 01:09:38 +0900

    2023.08.11 로그인 화면 UI 재구현

--- travelMemory/Login/LoginView.swift
+++ travelMemory/Login/LoginView.swift
@@ -12,21 +12,24 @@ class LoginView: UIView {
     public let logoImage = UIImageView(image: UIImage(named: "appLogo"))
     public let naverButton = NaverButton()
     public let kakaoButton = KakaoButton()
+    public let tmButton = TMButton()
     
     init() {
         super.init(frame: .zero)
         self.backgroundColor = UIColor.white
         
-        setupView(element: logoImage, top: 300, centerX: 0)
-        logoImage.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        logoImage.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        setupView(element: logoImage, top: 250, centerX: 0)
+        logoImage.widthAnchor.constraint(equalToConstant: 148).isActive = true
+        logoImage.heightAnchor.constraint(equalToConstant: 148).isActive = true
         
-        setupView(element: naverButton, left: 50)
-        naverButton.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 75).isActive = true
+        setupView(element: tmButton, right: -30, left: 30, centerX: 0)
+        tmButton.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 148).isActive = true
         
-        setupView(element: kakaoButton)
-        kakaoButton.leftAnchor.constraint(equalTo: naverButton.rightAnchor, constant: 25).isActive = true
-        kakaoButton.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 75).isActive = true
+        setupView(element: naverButton, right: -30, left: 30, centerX: 0)
+        naverButton.topAnchor.constraint(equalTo: tmButton.bottomAnchor, constant: 10).isActive = true
+        
+        setupView(element: kakaoButton, right: -30, left: 30, centerX: 0)
+        kakaoButton.topAnchor.constraint(equalTo: naverButton.bottomAnchor, constant: 10).isActive = true
     }
     
     required init?(coder: NSCoder) {
@@ -35,13 +38,34 @@ class LoginView: UIView {
     
 }
 
+class TMButton: UIButton {
+    
+    init(){
+        super.init(frame: .zero)
+        setTitle("일반 계정으로 시작하기", for: .normal)
+        setTitleColor(.black, for: .normal)
+        titleLabel?.font = UIFont(name: "Helvetica", size: 14)
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 1.0
+        
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class NaverButton: UIButton {
     
     init(){
         super.init(frame: .zero)
-        setBackgroundImage(UIImage(named: "naverButton"), for: .normal)
-        
-        widthAnchor.constraint(equalToConstant: 50).isActive = true
+        setTitle("네이버 계정으로 시작하기", for: .normal)
+        setTitleColor(.black, for: .normal)
+        titleLabel?.font = UIFont(name: "Helvetica", size: 14)
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 1.0
+
         heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
@@ -55,9 +79,12 @@ class KakaoButton: UIButton {
     
     init(){
         super.init(frame: .zero)
-        setBackgroundImage(UIImage(named: "kakaoButton"), for: .normal)
-        
-        widthAnchor.constraint(equalToConstant: 50).isActive = true
+        setTitle("카카오 계정으로 시작하기", for: .normal)
+        setTitleColor(.black, for: .normal)
+        titleLabel?.font = UIFont(name: "Helvetica", size: 14)
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 1.0
+
         heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
